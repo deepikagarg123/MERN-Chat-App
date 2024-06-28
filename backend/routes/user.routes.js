263373ea9d81c9e9e730a1eadee6b2ba2862f70a
deepikagarg2423 <deepikagarg2423@gmail.com>
2024-06-29 00:18:55 +0530

    1st part completed - backend

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import protectRoute from "../middleware/protectRoute.js";
+import { getUsersForSidebar } from "../controllers/user.controller.js";
+import User from "../models/user.model.js";
+
+const router=express.Router();
+
+
+
+router.get("/",protectRoute,getUsersForSidebar);
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,44 @@
+//package imports
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+// file imports
+import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+
+// variables
+const app=express();
+const PORT=process.env.PORT || 5000;
+
+dotenv.config();
+
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
+
+
+
+// routes
+// app.get("/",(req,res)=>{
+//     res.send("server is ready");
+// });
+
+
+
+
+
+app.listen(PORT,()=>{
+    connectToMongoDB();
+    console.log(`server running on port ${PORT}`)
+});
